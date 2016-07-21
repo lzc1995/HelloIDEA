5fff5ecfc54461aa35e3d0885b86183d9777acf2
Linzc <496814928@qq.com>
2016-07-21 13:35:18 +0800

    Initial commit

---
+++ HelloAPP/src/main/java/HelloMaven.java
@@ -0,0 +1,12 @@
+
+public class HelloMaven {
+
+    public String helloMaven() {
+        return "helloMaven";
+    }
+
+    public static void main(String[] args) {
+        HelloMaven helloMaven = new HelloMaven();
+        System.out.println(helloMaven.helloMaven());
+    }
+}
